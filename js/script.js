46fe8e79226699fdc51d5157758db389e28f0b3d
Aleksandr Suberlyak <sun_mail_86@mail.ru>
2016-05-08 15:44:39 +1000

    Добавил скрипты jquery и script
    Начал делать обработчик события нажатия кнопки обратной связи.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var link = document.querySelector(".btn-form");
+var popup = document.querySelector(".feedback");
+var close = document.querySelector(".feedback-close")
+link.addEventListener("click", function(event){
+  event.preventDefault();
+  popup.classList.add(".feedback-show");
+});
+close.addEventListener("click", function(event){
+  event.preventDefault();
+  popup.classList.remove(".feedback-show");
+});
