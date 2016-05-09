2bb4b63b7954e1e683757ec451160afe0ed4593c
Aleksandr Suberlyak <sun_mail_86@mail.ru>
2016-05-09 15:11:35 +1000

    Добавил тень для карточек с товарами. Начал делать эфферты для полей

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ var popup = document.querySelector(".feedback");
 var close = document.querySelector(".feedback-close")
 link.addEventListener("click", function(event){
   event.preventDefault();
-  popup.classList.add(".feedback-show");
+  popup.classList.add("feedback-show");
 });
 close.addEventListener("click", function(event){
   event.preventDefault();
-  popup.classList.remove(".feedback-show");
+  popup.classList.remove("feedback-show");
 });
